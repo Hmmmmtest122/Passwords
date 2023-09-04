a8b51626196a86f7e8d12cba3fa7c80a5a52fa16
Replit user <>
2023-09-04 14:57:33 +0000

    Added

--- game/main.js
+++ game/main.js
@@ -1,6 +1,13 @@
 alert("load")
 var a = document.getElementById("Test")
+function work() {
+  b.innerHTML = "<h1> sigur ca" + ss.value + "</h1>"
+}
 function abc() {
-  a.innerHTML = "<h1>de ce ai apasat</h1> <input> </input>"
+ document.innerHTML="<h1>de ce ai apsat</h1> <button id="."></button> <input id="3"></input>"
+var b = document.getElementById(".")
+var ss = document.getElementById("3")
+b.addEventListener("click")  
 }
 a.addEventListener("click",abc)
+ 
